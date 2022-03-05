5ff0de33e13b3f172a60c9cd0ee1b8fac131a245
tanim-k <tanimkhan877@gmail.com>
2022-03-06 03:47:29 +0600

    use of storaged data

--- shopping.js
+++ shopping.js
@@ -1,13 +1,55 @@
+const displayLocarStorageCart = () => {
+    
+    const cart = getCart();
+    for(const name in cart) {
+        displayProduct(name);
+    }
+}
 const addItem = () => {
+    /* document.getElementById('products').textContent = '' */
     const nameField = document.getElementById('product-name');
+    const name = nameField.value;
+    if(!name) {
+        return;
+    }
     // display in the ui
+    displayProduct(name)
     // add to local storage
-    // 
+    addProductToCart(name);
+    // clear
     nameField.value = ''; 
 }
 const displayProduct = name => {
     const ul = document.getElementById('products');
     const li = document.createElement('li');
-    li.innerText = name;
+    li.innerText = name.toLowerCase();
     ul.appendChild(li);
 }
+
+const getCart = () => {
+    const cart = localStorage.getItem('cart');
+    let cartObj;
+    if(cart){
+        cartObj = JSON.parse(cart);
+    }
+    else{
+        cartObj = {};
+    }
+    return cartObj;
+}
+const addProductToCart = name => {
+    const cart = getCart();
+    if(cart[name]){
+        cart[name] = cart[name]+1;
+    }
+    else{
+        cart[name] = 1;
+    }
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringified)
+}
+const placeOrder = () => {
+    document.getElementById('products').textContent = '';
+    localStorage.removeItem('cart');
+}
+displayLocarStorageCart();
