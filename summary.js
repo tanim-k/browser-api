97818cede0c9d9dc65d04de915f0ede4fc440bd5
tanim-k <tanimkhan877@gmail.com>
2022-03-06 03:53:58 +0600

    summary

---
+++ summary.js
@@ -0,0 +1,11 @@
+alert('');
+confirm('HOw are you');
+prompt('name');
+
+// location.reload()
+// location.href = ''
+// location.hash =
+// history.back()
+// history.go()
+// cokies
+// sessionStorage vs local Storage
